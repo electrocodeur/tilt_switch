c33479f67a52a1b990e06d7cc4bd39b2018432cd
electrocodeur <77399487+electrocodeur@users.noreply.github.com>
2021-03-06 23:02:23 +0100

    Ceci est le programme.

---
+++ tilt_switch.ino
@@ -0,0 +1,20 @@
+int tilt = 2;
+int led = 3;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(tilt, INPUT);
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (digitalRead(tilt) == HIGH) {
+    Serial.println("Allumer");
+    digitalWrite(led, HIGH);
+  }
+  if (digitalRead(tilt) == LOW) {
+    Serial.println("Eteint");
+    digitalWrite(led, LOW);
+  }
+}
